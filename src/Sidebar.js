2920be972bee454764070e8d8aead4d69ae81b0e
unknown <iarafat386@gmail.com>
2021-08-03 13:37:04 +0600

    Onclick Note Create functionality complete

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,28 +1,34 @@
 import React from 'react';
 
-const Sidebar = () => {
+const Sidebar = ({ notes, onAddNote }) => {
     return (
-       <div className="app-sidebar">
-           <div className="app-sidebar-header">
-               <h1>Notes</h1>
-               <button>Add</button>
-           </div>
-           {/* Notes */}
-           <div className="app-sidebar-notes">
-               <div className="app-sidebar-note">
-                   {/* Note title and button */}
-                   <div className="sidebar-note-title">
-                       <strong>TTITLE</strong>
-                       <button>Delete</button>
-                   </div>
-                   {/* Prview */}
-                   <p>Note preview</p>
-                   <small className="note-meta">
-                       Last modified [date]
-                   </small>
-               </div>
-           </div>
-       </div>
+        <div className="app-sidebar">
+            <div className="app-sidebar-header">
+                <h1>Notes</h1>
+                <button onClick={onAddNote}>Add</button>
+            </div>
+            {/* Notes */}
+            <div className="app-sidebar-notes">
+
+                {
+                    notes.map((note) => (
+                        <div className="app-sidebar-note">
+                            {/* Note title and button */}
+                            <div className="sidebar-note-title">
+                                <strong>TTITLE</strong>
+                                <button>Delete</button>
+                            </div>
+                            {/* Prview */}
+                            <p>Note preview</p>
+                            <small className="note-meta">
+                                Last modified [date]
+                            </small>
+                        </div>
+                    ))
+                }
+
+            </div>
+        </div >
     );
 };
 
